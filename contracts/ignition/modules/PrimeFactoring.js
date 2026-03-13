87173204a50888a60777756622d417b5a49a195b
Nola <81670189+Nolas-Shadow@users.noreply.github.com>
2026-03-13 17:04:55 -0400

    Rename Prime Factoring to Prime Invoice

--- contracts/ignition/modules/PrimeFactoring.js
+++ contracts/ignition/modules/PrimeFactoring.js
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import hre from "hardhat";
 
-export default buildModule("PrimeFactoringModule", (m) => {
+export default buildModule("PrimeInvoiceModule", (m) => {
     // 1. Deploy Mock USDC for the Testnet
     const initialSupply = hre.ethers.parseUnits("1000000", 6); // 1M USDC
     const mockUSDC = m.contract("MockERC20", ["USD Coin", "USDC", initialSupply]);
@@ -9,8 +9,8 @@ export default buildModule("PrimeFactoringModule", (m) => {
     // 2. We use the deployer's address as the initial owner of the protocol
     const protocolOwner = m.getAccount(0);
 
-    // 3. Deploy PrimeFactoring, passing the MockUSDC address and Owner
-    const primeFactoring = m.contract("PrimeFactoring", [mockUSDC, protocolOwner]);
+    // 3. Deploy PrimeInvoice, passing the MockUSDC address and Owner
+    const primeFactoring = m.contract("PrimeInvoice", [mockUSDC, protocolOwner]);
 
     return { mockUSDC, primeFactoring };
 });
